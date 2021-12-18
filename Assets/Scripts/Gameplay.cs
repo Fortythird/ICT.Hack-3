241f334931803f808c8ee99710fea087488f753e
Fortythird <evgeniikarmanovich@gmail.com>
2021-12-18 17:44:13 +0300

    Player's cards update
    - Player's card view has been added
    - Minor fixes

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
 public class Gameplay : MonoBehaviour
 {
     public Camera view;
 
-    public GameObject pos11, pos12, pos13, pos14, pos21, pos22, pos23, pos24, pos31, pos32, pos33, pos34;
+    public GameObject pos11, pos12, pos13, pos14, pos21, pos22, pos23, pos24, pos31, pos32, pos33, pos34,
+                      prefCrane, prefKoi, prefDragon, prefEmpty;
     private GameObject[,] field = new GameObject[4, 3];
+    private List<GameObject> avail_cards = new List<GameObject>();
+    private List<GameObject> types = new List<GameObject>();
 
     private bool step = false, pick_card = false, neko = true, onField = false, isDrag = false;
     private sbyte count = 0;
-    private short drag_start = 0, drag_finish = 0;
+    private short drag_start = 0, drag_finish = 0, cards_to_play = 2;
 
     void FieldView()
     {
@@ -31,12 +33,36 @@ public class Gameplay : MonoBehaviour
         }
     }
 
+    void CardPlacement()
+    {
+        int mod = avail_cards.Count % 2;
+        int div = avail_cards.Count / 2;
+        float x = 0;
+        Vector3 pos = new Vector3(x, 5.27f, -3.83f);
+        if (mod == 1)
+        {
+            Instantiate(avail_cards[0], pos, Quaternion.Euler(45, 0, 0));
+            for (int i = 1; i <= div; i++)
+            {
+                x = 0.2f * i;
+                Instantiate(avail_cards[i], pos, Quaternion.Euler(45, 0, 0));
+            }
+        }
+    }
+
     void Start()
     {
         field = new GameObject[4, 3] {{pos11, pos21, pos31},
                                       {pos21, pos22, pos23},
                                       {pos31, pos32, pos33},
-                                      {pos14, pos24, pos34}}; 
+                                      {pos14, pos24, pos34}};
+        types.Add(prefCrane);
+        types.Add(prefKoi);
+        types.Add(prefDragon);
+        short rand = (short)Random.Range(0, 3f);
+        avail_cards.Add(types[rand]);
+        rand = (short)Random.Range(0, 3f);
+        avail_cards.Add(types[rand]);
     }
 
     
@@ -44,6 +70,7 @@ public class Gameplay : MonoBehaviour
     {
         if (step)
         {
+
             if (Input.GetMouseButtonDown(0) && Input.mousePosition.y > 960f && !isDrag && !onField ||
                 Input.GetMouseButtonDown(0) && !isDrag && onField)
             {
